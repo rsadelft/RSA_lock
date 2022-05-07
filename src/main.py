a261beff3c7eb7f67b00eb9847617f694e3863fc
Ziang Qiu <ziang.qiu@gmail.com>
2022-05-07 16:07:21 +0200

    commit to master

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import update, env, lib.requests, lib.logger, lib.requests, lib.timew, time, os, machine
 from lib import base64
-import lib.service
-
+from lib.service import start
 t = lib.timew.Time(time=time)
 
 # Configure Logger
-logger = lib.logger.config(enabled=False, include=env.settings['logInclude'], exclude=env.settings['logExclude'], time=t)
+logger = lib.logger.config(enabled=False, include=env.settings['logInclude'], exclude=env.settings['logExclude'],
+                           time=t)
 log = logger(append='boot')
 log("The current time is %s" % t.human())
 
@@ -13,20 +13,19 @@ loggerOta = logger(append='OTAUpdater')
 
 io = update.IO(os=os, logger=loggerOta)
 github = update.GitHub(
-  io=io,
-  remote=env.settings['githubRemote'],
-  branch=env.settings['githubRemoteBranch'],
-  logger=loggerOta,
-  requests=lib.requests,
-  username=env.settings['githubUsername'],
-  token=env.settings['githubToken'],
-  base64=base64,
+    io=io,
+    remote=env.settings['githubRemote'],
+    branch=env.settings['githubRemoteBranch'],
+    logger=loggerOta,
+    requests=lib.requests,
+    username=env.settings['githubUsername'],
+    token=env.settings['githubToken'],
+    base64=base64,
 )
 updater = update.OTAUpdater(io=io, github=github, logger=loggerOta, machine=machine)
-
 try:
-  updater.update()
+    updater.update()
 except Exception as e:
-  log('Failed to OTA update:', e)
+    log('Failed to OTA update:', e)
 
-lib.service.start()
+start()
