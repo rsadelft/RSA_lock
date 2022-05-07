95e48a51dbd010b3d405c8fea7f3b599a12fee96
Ziang Qiu <ziang.qiu@gmail.com>
2022-05-07 18:30:02 +0200

    commit to master

--- src/lib/service.py
+++ src/lib/service.py
@@ -1,8 +1,12 @@
-from machine import Pin, SPI
+from machine import Pin, PWM
 import time
 def start():
     print("123456789123456789")
     p0 = Pin(21, Pin.OUT)
+    p1 = PWM(15)
     p0.on()
-    time.sleep(1)
+    p1.freq(500)
+    p1.duty(512)
+    time.sleep(5)
     p0.off()
+    p1.deinit()
